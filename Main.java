8b6fca0d5db7fd1581c3afe2e516ca84feb4c4a0
deckelsl <deckelsl@mail.uc.edu>
2020-09-14 20:26:24 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+
+  public static void main(String[] args) {
+  Scanner s = new Scanner(System.in);
+    System.out.println("Enter a number to count down or up from:");
+    int number = s.nextInt();
+    if (number > 0) {
+      for (int i=number; i>=0; i--)
+      {
+        System.out.println(i);
+      }
+    }
+    if (number < 0) {
+      for (int i=number; i<=0; i++) {
+        System.out.println(i);
+      }
+    }
+    System.out.println("Blast off!");
+  }
+}
